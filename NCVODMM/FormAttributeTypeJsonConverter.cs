67181e1f2f4d088e3c26f06bae898ace1d59f89c
ben sheridan <ben.sheridan@reasondigital.com>
2018-05-18 09:53:36 +0100

    first commit basic concept

---
+++ NCVODMM/FormAttributeTypeJsonConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NCVODMM.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace NCVODMM
+{
+	public class FormAttributeTypeJsonConverter : JsonConverter
+	{
+		public override bool CanConvert(Type objectType)
+		{
+			return objectType == typeof(FormAttribute);
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			JObject jsonObject = JObject.Load(reader);
+
+			Type type = Type.GetType("NCVODMM.Models." + jsonObject["Type"]);
+
+			return JsonConvert.DeserializeObject(jsonObject.ToString(), type);
+
+			return null;
+		}
+
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ NCVODMM/Models/Form.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace NCVODMM.Models
+{
+    public class Form
+    {
+
+		public string Name { get; set; }
+
+		[JsonProperty(ItemConverterType = typeof(FormAttributeTypeJsonConverter))]
+		public List<FormAttribute> Attributes { get; set; }
+
+	}
+}
